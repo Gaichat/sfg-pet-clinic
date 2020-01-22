5dc777977ce027773110b80aab97f8cbcea98229
maeliss vns <maeliss.avenoso@gmail.com>
2020-01-22 22:01:49 +0100

    add POJO

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class PetType {
+    private String typeName;
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+}
