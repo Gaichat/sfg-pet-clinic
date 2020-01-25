a1eda8687438aad5c304b8623e0eb44e51e9d8fc
maeliss vns <maeliss.avenoso@gmail.com>
2020-01-25 18:03:28 +0100

    correct typo

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
@@ -5,7 +5,7 @@ import java.util.Set;
 public interface CrudService <T,ID> {
     T findById(ID id);
     Set<T> findAll();
-    T save(T t);
-    void delete (T t);
+    T save(T object);
+    void delete (T object);
     void deleteById(ID id);
 }
