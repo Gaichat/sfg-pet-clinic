143a2fe6400e88b08178935a7b6b5b3417128976
maeliss vns <maeliss.avenoso@gmail.com>
2020-02-05 13:02:28 +0100

    add Owner to JPA. closes #29

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,6 +1,14 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pet_types")
 public class PetType extends BaseEntity{
+
+    @Column(name = "type_name")
     private String typeName;
 
     public String getTypeName() {
